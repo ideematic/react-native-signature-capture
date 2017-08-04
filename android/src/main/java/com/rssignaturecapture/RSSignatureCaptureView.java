df3e964fb3dcb038d292cfec17501983d9eed7be
Fabrice Didierjean <f.didierjean@ideematic.com>
2017-08-04 12:01:53 +0200

    change StrokeLineSize max

--- android/src/main/java/com/rssignaturecapture/RSSignatureCaptureView.java
+++ android/src/main/java/com/rssignaturecapture/RSSignatureCaptureView.java
@@ -87,7 +87,9 @@ public class RSSignatureCaptureView extends View {
 	* @return
 	*/
 	public void setStrokeLineSize(Integer strokeLineSize) {
-		mMinWidth = convertDpToPx((float)strokeLineSize);
+		Log.d("RSSignatureCaptureView::setStrokeLineSize", "" + strokeLineSize)
+		mMinWidth = convertDpToPx(strokeLineSize);
+		mMaxWidth = convertDpToPx(strokeLineSize + 3);
 	}
 
 	/**
